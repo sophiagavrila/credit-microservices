b03e39a1490fddbbcd5c8a7b75bbecca19763338
sophiagavrila <msophiagavrila@gmail.com>
2021-10-10 15:17:56 -0400

    add all base src code for config

--- phase3/cards/src/main/java/com/revature/cards/controller/CardsController.java
+++ phase3/cards/src/main/java/com/revature/cards/controller/CardsController.java
@@ -3,12 +3,18 @@ package com.revature.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.revature.cards.config.CardsServiceConfig;
 import com.revature.cards.model.Cards;
 import com.revature.cards.model.Customer;
+import com.revature.cards.model.Properties;
 import com.revature.cards.repository.CardsRepository;
 
 @RestController
@@ -16,6 +22,9 @@ public class CardsController {
 	
 	@Autowired
 	private CardsRepository cardsRepository;
+	
+	@Autowired
+	CardsServiceConfig cardsConfig;
 
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -27,6 +36,16 @@ public class CardsController {
 		} else {
 			return null;
 		}
-
 	}
+
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+				cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+	
+	
 }
